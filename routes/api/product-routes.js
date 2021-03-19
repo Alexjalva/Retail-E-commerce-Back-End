f90c0b21c2ef3b43b188a1cb0efb4462b3ae0786
Alex Alvarado <Alexjalva@gmail.com>
2021-03-18 18:29:50 -0700

    pushing initial code modifications, models complete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const prodsData = await Product.findAll({include: [
+      {model: Category, attributes: ['category_name']},
+      {model: Tag, attributes: ['tag_name']}
+    ]});
+    res.status(200).json(prodsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+    const prodData = await Product.findByPk(req.params.id, {
+      include: [
+        {model:Category, attributes:['category_name']},
+        {model: Tag, attributes: ['tag_name']}
+      ]
+    });
+    if(!prodData){
+      res.status(404).json({ message:'No traveller found with this id!'});
+      return;
+    }
+    res.status(200).json(prodData);
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 // create new product
